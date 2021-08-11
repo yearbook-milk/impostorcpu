ca7d39fef1a28905349ae67d696398ff29055402
Charles <infoforchacha@gmail.com>
2021-08-10 20:57:54 -0400

    added math commands

--- htm_bridge/bridger.py
+++ htm_bridge/bridger.py
@@ -36,6 +36,58 @@ for i in instructions:
         print('0D STOP')
     if cmd == 'remark':
         tr = ''
+
+    if cmd == 'add   ':
+        litadr = ins.split(' | ')
+        params = litadr[0].split('<-')
+        #print(litadr)
+        if litadr[1] == 'lit':
+            print('02*00 ADD: ',params[0],'+',params[1])
+            tr = tr + '{02 00 00} {'+formats(params[0])+'} {'+formats(params[1])+'}'
+            #print(tr)
+        if litadr[1] == 'adr':
+            print('02*01 ADD: ',params[0],'+',params[1])
+            tr = tr + '{02 00 01} {'+formats(params[0])+'} {'+formats(params[1])+'}'
+            #print(tr)
+
+    if cmd == 'sub   ':
+        litadr = ins.split(' | ')
+        params = litadr[0].split('<-')
+        #print(litadr)
+        if litadr[1] == 'lit':
+            print('03*00 SUB: ',params[0],'-',params[1])
+            tr = tr + '{03 00 00} {'+formats(params[0])+'} {'+formats(params[1])+'}'
+            #print(tr)
+        if litadr[1] == 'adr':
+            print('03*01 SUB: ',params[0],'-',params[1])
+            tr = tr + '{03 00 01} {'+formats(params[0])+'} {'+formats(params[1])+'}'
+            #print(tr)
+
+    if cmd == 'mul   ':
+        litadr = ins.split(' | ')
+        params = litadr[0].split('<-')
+        #print(litadr)
+        if litadr[1] == 'lit':
+            print('04*00 ADD: ',params[0],'*',params[1])
+            tr = tr + '{04 00 00} {'+formats(params[0])+'} {'+formats(params[1])+'}'
+            #print(tr)
+        if litadr[1] == 'adr':
+            print('04*01 ADD: ',params[0],'*',params[1])
+            tr = tr + '{04 00 01} {'+formats(params[0])+'} {'+formats(params[1])+'}'
+            #print(tr)
+
+    if cmd == 'div   ':
+        litadr = ins.split(' | ')
+        params = litadr[0].split('<-')
+        #print(litadr)
+        if litadr[1] == 'lit':
+            print('05*00 ADD: ',params[0],'/',params[1])
+            tr = tr + '{05 00 00} {'+formats(params[0])+'} {'+formats(params[1])+'}'
+            #print(tr)
+        if litadr[1] == 'adr':
+            print('05*01 ADD: ',params[0],'/',params[1])
+            tr = tr + '{05 00 01} {'+formats(params[0])+'} {'+formats(params[1])+'}'
+            #print(tr)
         
     endtotal = endtotal + tr + '\n'
     print('\n')
