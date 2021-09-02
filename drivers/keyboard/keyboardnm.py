a6d4bc0ca392c984effb2efa61a69aac5a36cba4
Charles <infoforchacha@gmail.com>
2021-09-01 21:25:41 -0400

    added keyboard interface

---
+++ drivers/keyboard/keyboardnm.py
@@ -0,0 +1,34 @@
+import tkinter as tk
+
+def fileio(name, mode, contents = ""): #file io helper
+    f = open(name, mode)
+    if mode == 'r':
+        tr = f.read()
+    if mode == 'a' or mode == 'w':
+        f.write(contents)
+        tr = None
+    f.close()
+    return tr
+    
+def callbackfunc(key):
+    print(chr( int(key,16) ))
+    fileio('bus1in.bus', 'w', key)
+
+buttons = []
+
+root = tk.Tk()
+buttons.append(tk.Button(text='1 UP    ', command=lambda: callbackfunc('31')))
+buttons.append(tk.Button(text='2 DOWN  ', command=lambda: callbackfunc('32')))
+buttons.append(tk.Button(text='3 LEFT  ', command=lambda: callbackfunc('33')))
+buttons.append(tk.Button(text='4 RIGHT ', command=lambda: callbackfunc('34')))
+buttons.append(tk.Button(text='5 ENTER ', command=lambda: callbackfunc('35')))
+buttons.append(tk.Button(text='6 CANCEL', command=lambda: callbackfunc('36')))
+buttons.append(tk.Button(text='7       ', command=lambda: callbackfunc('37')))
+buttons.append(tk.Button(text='8       ', command=lambda: callbackfunc('38')))
+buttons.append(tk.Button(text='9       ', command=lambda: callbackfunc('39')))
+buttons.append(tk.Button(text='0       ', command=lambda: callbackfunc('30')))
+
+for i in buttons:
+    i.pack()
+    
+root.mainloop()
