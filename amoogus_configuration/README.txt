101557dcee3d5e521ed02799e2eea921f6d60d83
Charles <infoforchacha@gmail.com>
2021-09-07 12:50:00 -0400

    so. it ends like this.

---
+++ amoogus_configuration/README.txt
@@ -0,0 +1,4 @@
+after months of work and hours straight of configuring a program
+the amoogus drawing tool is compolete and the one very sus boi has been drawn.
+
+my back hurts
---
+++ amoogus_configuration/monitor_api.py
@@ -0,0 +1,99 @@
+#basic CV2 based display API (for the driver)
+import cv2
+import numpy as np
+
+#clear output file
+f = open('monitor_out.png','wb')
+f.close()
+
+
+debug = False
+white = (255,255,255)
+black = (0,0,0)
+ctx = ''
+
+#inititialize image
+img = cv2.imread('monitor.png')
+
+if debug:
+    cv2.imshow('monitorout', img)
+    cv2.waitKey(0)
+print('Monitor API imported')
+
+height, width, channels = img.shape
+
+def imgwrite(text):
+    global ctx, debug, img, white, black
+    ctx = ctx + text
+    
+    if text == '\n':
+        ctx = ctx + ''
+        return None
+
+
+    items = ctx.split('\n')
+    index = 20
+    for i in items:
+        img = cv2.putText(img, i, (0,index), cv2.FONT_HERSHEY_SIMPLEX, 0.5, white)
+        index = index + 20
+
+    if debug:
+        cv2.imshow('monitorout', img)
+        cv2.waitKey(0)
+
+    cv2.imwrite('monitor_out.png', img)
+
+def deimgwrite(text):
+    global ctx, debug, img, white, black
+    ctx = ctx + text
+    
+    if text == '\n':
+        ctx = ctx + ''
+        return None
+
+    items = ctx.split('\n')
+    index = 20
+    for i in items:
+        img = cv2.putText(img, i, (0,index), cv2.FONT_HERSHEY_SIMPLEX, 0.5, black)
+        index = index + 20
+
+    if debug:
+        cv2.imshow('monitorout', img)
+        cv2.waitKey(0)
+
+    cv2.imwrite('monitor_out.png', img)
+
+def pxwrite(right,down,thic=3):
+    print('pxwrite()',right,down,thic)
+    global debug, img, white, black
+    img[down*thic-thic:down*thic+thic,right*thic-thic:right*thic+thic] = white
+
+    if debug:
+        cv2.imshow('monitorout', img)
+        cv2.waitKey(0)
+
+    cv2.imwrite('monitor_out.png', img)
+
+def depxwrite(right,down,thic=3):
+    print('depxwrite()',right,down,thic)
+    global debug, img, white, black
+    img[down*thic-thic:down*thic+thic,right*thic-thic:right*thic+thic] = black
+
+    if debug:
+        cv2.imshow('monitorout', img)
+        cv2.waitKey(0)
+        
+def imageblank(color = black):
+    global img, ctx, black, white
+    img[0:height,0:width] = black
+    ctx = ''
+    
+def imagewhiteblank(color = white):
+    global img, ctx, black, white
+    img[0:height,0:width] = white
+    ctx = ''
+    
+
+    cv2.imwrite('monitor_out.png', img)
+
+imgwrite('cv2 Basic Display API 1: on-startup self test successful\n4inches-usbstick/impostorcpu/drivers/monitor/monitor_api.py\n')
